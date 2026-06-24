3d4fcbb36490db3d168563632a1eba9044f37182
glxt0312-lgtm <glxt0312@gmail.com>
2026-06-24 14:29:00 +0300

    data_load

---
+++ frontend/main.py
@@ -0,0 +1,17 @@
+#$ pip install streamlit
+#$ streamlit hello
+
+import streamlit as st 
+
+st.set_page_config("Первый проект",layout='wide')
+
+st.title("Привет, streamlit")
+
+# name = st.text_input("Enter your name:", placeholder="Student")
+
+# number = st.slider("Choise the number:", 0, 10, 5)
+
+# if st.button("Считаем квадрат"):
+#   result = number ** 2
+#   st.success(f"Привет, {name}, Квадрат числа {number} = {result}")
+#   st.balloons()
